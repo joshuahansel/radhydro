eb9454485eb6bf231795c3e9d5a2434572f6c6e4
jhansel <joshua.hansel89@gmail.com>
2015-05-05 16:07:07 -0500

    started ss radiation solver, made changes to main

--- trunk/main.py
+++ trunk/main.py
@@ -13,36 +13,16 @@ from src.radiationSolver import *
 #  This function does main stuff.
 def main():
 
-    #Create a mesh, currently hardcoded
+    # create uniform mesh
     mesh = Mesh(10, 5.)
-    print mesh
 
+    # compute cross sections
+    cross_sects = [(CrossXInterface(1,3), CrossXInterface(1,3)) for i in xrange(mesh.n_elems)]
 
-    #Dear josh: this will Test out inv cubed cross X. Derived classes are a little wierd in python, you
-    #you just deal with derived class types, and any bass class function or members
-    #it needs it will have access to, as long as the constructors are called
-    #correctly. 
-    print "\n The fancy cross sections are"
-    cross_sects = [InvCubedCrossX(5.0,i+1,i+1,scale_coeff=10.0) for i in
-            xrange(mesh.n_elems)]
-    
-    for i in cross_sects:
-        print i
-
-    
-    #Dear Josh: These are normal cross sections, these are all you need to deal with
-    #in the S2 Solver. The above was just me learning how inheritence works in python,
-    #but we shouldnt really need to deal with it other than cross X's
-    cross_sects = [CrossXInterface(1,3) for i in xrange(mesh.n_elems)]
-    print "\nCross sections are: "
-    for i in cross_sects:
-        print i
-
-    #Call radiation solver
-    Q0 = 0.0
-    Q1_plus = 0.0
-    Q1_minus = 0.0
-    psi_minus, psi_plus, E, F = radiationSolver(mesh, cross_sects, Q0, Q1_plus, Q1_minus)
+    # call radiation solver
+    Q_plus  = [(0,0) for i in xrange(mesh.n_elems)]
+    Q_minus = [(0,0) for i in xrange(mesh.n_elems)]
+    psi_minus, psi_plus, E, F = radiationSolver(mesh, cross_sects, Q_minus, Q_plus)
 
 
 # run main function
--- trunk/src/radiationSolver.py
+++ trunk/src/radiationSolver.py
@@ -1,39 +1,37 @@
+import math
 import numpy as np
 from numpy import array
 from Mesh import Mesh
 
-#------------------------------------------------------------------------------------
-#"""Input is the mesh, list of cross sections for each element, Q0 isotropic source,
-#Q1 source for plus directions, and Q1_minus direction source.  Keyword arguments
-#passed in are the boundary currents, which are assumed vacuum by default, and the
-#scale_factor and diag_add_term. 
+## Index function for 1-D LD S-2.
 #
-#CX's and all sources are passed in as a list, where the index corresponds to the
-#element id. They are all passed in as a tuple with the left and right value, e.g.,
-#cx[0] = (cx_l,0, cx_r,0).
+#  @param[in] i     cell index, from 1 to n-1, where n is number of cells
+#  @param[in] side  string, either "L" or "R", corresponding to left or right dof
+#  @param[in] dir   string, either "-" or "+", corresponding to - or + direction
 #
-#stream_scale_factor: the streaming scale factor (call it alpha) multiplies the
-#reaction and the entire streaming term, i.e., sigma_t psi + mu \dpsi/dx ->
-#alpha*(sigma_t psi + mu \dpsi/dx).  This is for used with various time dependent
-#solvers.
-#
-#diag_add_term: added to the reaction term in each equation. For use in time-dependent
-#solvers, e.g., alpha*sigma_t*psi_L -> (alpha*sigma_t + 1/c*delta_t)psi_L. Must be done after scale
-#factor is applied.
-#
-#"""
-#  
-#
+#  @return    global dof index
+def index(i, side, dir):
+  side_shift = {"L" : 0, "R" : 2}
+  dir_shift  = {"-" : 0, "+" : 1}
+  return 4*i + side_shift[side] + dir_shift[dir]
 
 ## Steady-state solve function for the S-2 equations.
 #
 #  @param[in] mesh     a mesh object
-#  @param[in] cross_x  list of cross sections for each element
-#  @param[in] Q0       isotropic source
-#  @param[in] Q1_plus  source for plus directions
-#  @param[in] Q1_minus source for minus directions
-#  @param[in] stream_scale_factor scale factor for reaction and streaming terms
-#  @param[in] diag_add_term       term to add to reaction term
+#  @param[in] cross_x  list of cross sections for each element, stored as tuple
+#                      for each cell.
+#  @param[in] Q_minus  total isotropic source in minus direction:
+#                      \f$Q = Q_0 + 3\mu^-Q_1\f$
+#  @param[in] Q_plus   total isotropic source in plus direction:
+#                      \f$Q = Q_0 + 3\mu^+Q_1\f$
+#  @param[in] stream_scale_factor scale factor \f$\alpha\f$for reaction and
+#                                 streaming terms; sigma_t psi + mu \dpsi/dx ->
+#                                 alpha*(sigma_t psi + mu \dpsi/dx). This is
+#                                 used with various time dependent solvers.
+#  @param[in] diag_add_term       term to add to reaction term for use in
+#                                 time-dependent solvers, e.g., alpha*sigma_t*psi_L
+#                                 -> (alpha*sigma_t + 1/c*delta_t)psi_L. Must be
+#                                 done after scale
 #  @param[in] bound_curr_lt       left boundary current
 #  @param[in] bound_curr_rt       right boundary current
 #
@@ -43,23 +41,59 @@ from Mesh import Mesh
 #          -# \f$\mathcal{E}\f$: radiation energy
 #          -# \f$\mathcal{F}\f$: radiation flux
 #
-def radiationSolver(mesh, cross_x, Q0, Q1_plus, Q1_minus, stream_scale_factor=1.0,
+def radiationSolver(mesh, cross_x, Q_minus, Q_plus, stream_scale_factor=1.0,
         diag_add_term=0.0, bound_curr_lt=0.0, bound_curr_rt=0.0):
 
+    # set directions
+    mu = {"-" : -1/math.sqrt(3), "+" : 1/math.sqrt(3)}
+
+    # 1/(4*pi) constant for isotropic source Q
+    c_Q = 1/(4*math.pi)
+
+    # initialize numpy arrays for system matrix and rhs
+    n = 4*mesh.n_elems
+    matrix = np.zeros((n, n))
+    rhs    = np.zeros(n)
+
+    # loop over interior cells
+    for i in xrange(1,mesh.n_elems-1):
+       # compute indices
+       iprevRplus  = index(i-1,"R","+") # dof i-1,R,+
+       iLminus     = index(i,  "L","-") # dof i,  L,-
+       iLplus      = index(i,  "L","+") # dof i,  L,+
+       iRminus     = index(i,  "R","-") # dof i,  R,-
+       iRplus      = index(i,  "R","+") # dof i,  R,+
+       inextLminus = index(i+1,"L","-") # dof i+1,L,-
+
+       # get cell size
+       h = mesh.getElement(i).dx
+
+       # get cross sections
+       cx_sL = cross_x[i][0].sig_s # Left  scattering
+       cx_sR = cross_x[i][1].sig_s # Right scattering
+       cx_tL = cross_x[i][0].sig_t # Left  total
+       cx_tR = cross_x[i][1].sig_t # Right total
+
+       # get sources
+       QLminus = Q_minus[i][0] # minus direction, Left
+       QLplus  = Q_plus [i][0] # plus  direction, Left
+       QRminus = Q_minus[i][1] # minus direction, Right
+       QRplus  = Q_plus [i][1] # plus  direction, Right
+
+       # Left control volume, minus direction
+       row = np.zeros(n)
+       row[iLminus] = -0.5*mu["-"] + 0.5*cx_tL*h - 0.25*cx_sL*h
+       row[iLplus]  = -0.25*cx_sL*h
+       row[iRminus] = 0.5*mu["-"]
+       matrix[iLminus] = row
+       rhs[iLminus] = 0.5*c_Q*h*QLplus
+      
 
 
-
-
-
-    psi_l = 0.0
-    psi_r = 0.0
-
-
-    psi_minus = [(psi_l, psi_r) for i in range(mesh.n_elems)]
-    psi_plus = [(psi_l, psi_r) for i in range(mesh.n_elems)]
-    E = [(psi_l, psi_r) for i in range(mesh.n_elems)]
-    F = [(psi_l, psi_r) for i in range(mesh.n_elems)]
+    # return zeros for time being
+    psi_minus = [(0, 0) for i in range(mesh.n_elems)]
+    psi_plus = [(0, 0) for i in range(mesh.n_elems)]
+    E = [(0, 0) for i in range(mesh.n_elems)]
+    F = [(0, 0) for i in range(mesh.n_elems)]
 
     return psi_minus, psi_plus, E, F
-
-
