ba3f0d8a0396875348756e105c9adc0dfb7f0498
sbolding <simonrbolding@gmail.com>
2015-05-12 12:48:32 -0500

    Added some better comments. Josh, look at how temperature is treated.  It can be very confusing

--- trunk/src/globalConstants.py
+++ trunk/src/globalConstants.py
@@ -7,17 +7,46 @@
 # a = GC.RAD_CONST #get radiation constant
 # c = GC.SPD_OF_LIGHT #get speed of light, etc.
 #
+# Our standard units are length - cm, time - shakes (sh),
+# energy and temperature - keV, mass - g, volume - cm^3
 #
+# Temperatures are done as an equivalent temperature, so 
+# 
+# T = T (K) * Boltzmann Constant (keV/K)
+# 
+# All constants will are in units so that temperatures can just be treated as in 
+# keV.  This is confusing for specific heats, which will have units of
+#
+# C_V (energy/(temp * volume)) has units (keV/(keV*cm^3))
 
 # All constants are at global scope in this module, please dont change them :)
 
-SH_PER_S = 1.E+8  #shakes per second
-SPD_OF_LGT = 299.792458L #Units of cm/shake
-BOLTZMAN_CONSTANT = 8.617343E-08 #keV/K
-RAD_CONSTANT = 0.01372 #grey radiation constant, a, in jk/(cm^3*keV^4)
-JK_PER_JOULES = 1.E-09 #jk/J ("jerks" per Joules)
-EV_PER_KEV = 1000. #eV/keV
+##shakes per second
+SH_PER_S = 1.E+8  
+
+##Speed of light in units of cm/shake
+SPD_OF_LGT = 299.792458L 
+
+##Boltzman constant k in keV/K, for converting to equivalent energy of T
+BOLTZMAN_CONSTANT = 8.617343E-08 
+
+##grey radiation constant, a, in jk/(cm^3*keV^4)
+RAD_CONSTANT = 0.01372 
+
+##jk/J ("jerks" per Joules)
+JK_PER_JOULES = 1.E-09 
+
+##eV/keV
+EV_PER_KEV = 1000. 
+
+##Joules per KEV to convert some specific heat constants
 JOULES_PER_KEV = 1.602176565e-16
-JOULES_PER_ERG = 1.E-07 #Some times specific heats are given in ergs
+
+##Some times specific heats are given in ergs
+JOULES_PER_ERG = 1.E-07
+
+##To convert from jerks to energy in KEV, our standard energy unit is KEV
 JK_PER_KEV = 1.602176565e-25
-PLANCKS_CONSTANT = 6.6260693E-35 #jk-sh
+
+##Plancks constant in jk-sh
+PLANCKS_CONSTANT = 6.6260693E-35 
