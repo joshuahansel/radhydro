BDF2 works, fixed bugs with duplicate kw arguments. Also there was a strange casting issue from a np.array to a list in the CN that was causing the minor numerical error b/w the steady state and transient solution at shorter times (I think). CN now reaches steady state to roundoff as BE and BDF2 do

trunk/src/radExecutioner.py
@@ -27,9 +27,9 @@ def solveRadProblem():
                   for i in xrange(mesh.n_elems)]
 
     # transient options
-    dt = 0.05
+    dt = 0.1
     t  = 0.0
-    t_end = 100.
+    t_end = 10.
 
     # create the steady-state source
     Q_iso = 5.
@@ -66,11 +66,15 @@ def solveRadProblem():
     psi_m_i = psi_minusSS[mesh.n_elems/2][0]
     psi_plus_old  = [(psi_p_i,psi_p_i) for i in range(mesh.n_elems)]
     psi_minus_old = [(psi_m_i,psi_m_i) for i in range(mesh.n_elems)] 
+    psi_plus_older = [(0.5*i[0], 0.5*i[1]) for i in psi_plus_old ]
+    psi_minus_older = deepcopy(psi_plus_older)
     E_old = [(1./GC.SPD_OF_LGT*(psi_plus_old[i][0] + psi_minus_old[i][0]),
               1./GC.SPD_OF_LGT*(psi_plus_old[i][1] + psi_minus_old[i][1]))  for i in
             range(len(psi_plus_old))]
+    E_older = [(1./GC.SPD_OF_LGT*(psi_plus_older[i][0] + psi_minus_older[i][0]),
+              1./GC.SPD_OF_LGT*(psi_plus_older[i][1] + psi_minus_older[i][1]))  for i in
+            range(len(psi_plus_old))]
 
-    psi_plus_older = psi_plus_old[:]
 
     #phiSS = computeScalarFlux(psi_plusSS, psi_minusSS)
     #plotScalarFlux(mesh, psi_minus_old, psi_plus_old, scalar_flux_exact=phiSS,
@@ -98,12 +102,16 @@ def solveRadProblem():
         Q_tot = np.array(Q)
         for src in source_handles:
             # build src for this handler
-            Q_src = src.buildSource(psi_plus_old  = psi_plus_old,
-                                    psi_minus_old = psi_minus_old,
-                                    bc_flux_left  = psi_left,
-                                    bc_flux_right = psi_right,
-                                    cx_old        = cross_sects,
-                                    E_old         = E_old)
+            Q_src = src.buildSource(psi_plus_old    = psi_plus_old,
+                                    psi_minus_old   = psi_minus_old,
+                                    psi_minus_older = psi_minus_older,
+                                    psi_plus_older  = psi_plus_older,
+                                    bc_flux_left    = psi_left,
+                                    bc_flux_right   = psi_right,
+                                    cx_old          = cross_sects,
+                                    cx_older        = cross_sects,
+                                    E_old           = E_old,
+                                    E_older         = E_older)
             # Add elementwise the src to the total
             Q_tot += Q_src
 
@@ -112,10 +120,17 @@ def solveRadProblem():
                 bc_psi_left = psi_left, bc_psi_right = psi_right, diag_add_term =
                 react_term, implicit_scale = diag_terms[ts] )
 
+        # save oldest solutions
+        psi_plus_older  = deepcopy(psi_plus_old)
+        psi_minus_older = deepcopy(psi_minus_old)
+        E_older         = deepcopy(E_old)
+
         # save old solutions
         psi_plus_old  = deepcopy(psi_plus)
         psi_minus_old = deepcopy(psi_minus)
         E_old         = deepcopy(E)
+
+
 
         #print sum([(psi_plus[i][0]- psi_plusSS[i][0])/psi_plus[i][0] for i in range(len(psi_plus_old))])
 

trunk/src/sourceHandlers.py
@@ -199,7 +199,7 @@ class StreamingSrc(SourceHandler):
     #  of equation is already included in the system
     def evalImplicit(self, el, **kwargs):
 
-        return [0.0 for i in xrange(4)]
+        return np.zeros(4)
 
     #--------------------------------------------------------------------------------
     ## At time \f$t_n\f$, there is a streaming source based on upwinding in
@@ -256,8 +256,9 @@ class StreamingSrc(SourceHandler):
     #  version
     def evalOlder(self, el, psi_minus_older=None, psi_plus_older=None, **kwargs):
 
-        return self.evalOld(self,el,psi_minus_old=psi_minus_older,
-                psi_plus_old=psi_plus_older)
+        #have to carefully pass in **kwargs to avoid duplicating
+        return self.evalOld(el,psi_minus_old=psi_minus_older,psi_plus_old=psi_plus_older,
+                bc_flux_left=kwargs['bc_flux_left'],bc_flux_right=kwargs['bc_flux_right'])
 
 
 #====================================================================================
@@ -275,7 +276,7 @@ class ReactionSrc(SourceHandler):
     #  of equation is already included in the system
     def evalImplicit(self, el, **kwargs):
 
-        return [0.0 for i in xrange(4)]
+        return np.zeros(4)
 
     #--------------------------------------------------------------------------------
     ## At time \f$t_n\f$, there is a reaction source, straight forward
@@ -303,12 +304,12 @@ class ReactionSrc(SourceHandler):
 
     #--------------------------------------------------------------------------------
     ## The older term is the same as old, but with the oldest fluxes. So call old
-    #  version
+    #  version. You cannot pass in **kwargs or it will duplicate some arguments
     def evalOlder(self, el, psi_minus_older=None, psi_plus_older=None, 
                    cx_older = None, **kwargs):
 
-        return self.evalOld(self,el,psi_minus_old=psi_minus_older,
-                psi_plus_old=psi_plus_older,cx_old=cx_older,**kwargs)
+        return self.evalOld(el,psi_minus_old=psi_minus_older,
+                psi_plus_old=psi_plus_older,cx_old=cx_older)
 
 #====================================================================================
 ## Derived class for scattering source term:  \sigma_s phi/2
@@ -326,12 +327,11 @@ class ScatteringSrc(SourceHandler):
     #  of equation is already included in the system
     def evalImplicit(self, el, **kwargs):
 
-        return [0.0 for i in xrange(4)]
+        return np.zeros(4)
 
     #--------------------------------------------------------------------------------
     ## At time \f$t_n\f$, there is a isotropic scattering source
-    def evalOld(self, i, E_old=None, cx_old = None, psi_minus_old=None,
-            psi_plus_old=None, **kwargs):
+    def evalOld(self, i, E_old=None, cx_old = None, **kwargs):
 
         #Get scattering cross section
         sig_s_l = cx_old[i][0].sig_s
@@ -359,5 +359,5 @@ class ScatteringSrc(SourceHandler):
     #  version
     def evalOlder(self, el, E_older = None, cx_older = None, **kwargs):
 
-        return self.evalOld(self,el,E_old = E_older,cx_old=cx_older,**kwargs)
+        return self.evalOld(el,E_old = E_older,cx_old=cx_older)
 