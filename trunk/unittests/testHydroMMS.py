0f17e7cef0f2b4ef60d1a9137817c310f9302869
Joshua Hansel <joshua.hansel89@gmail.com>
2015-06-16 11:56:23 -0500

    added hydro-only version of MMS test

---
+++ trunk/unittests/testHydroMMS.py
@@ -0,0 +1,148 @@
+## @package testHydroMMS
+#  Contains unittest class to test an MMS problem with only hydrodynamics
+
+# add source directory to module search path
+import sys
+sys.path.append('../src')
+
+# symbolic math packages
+from sympy import symbols, exp, sin, pi
+from sympy.utilities.lambdify import lambdify
+
+# numpy
+import numpy as np
+
+# unit test package
+import unittest
+
+# local packages
+from createMMSSourceFunctions import createMMSSourceFunctionsRadHydro
+from mesh import Mesh
+from hydroState import HydroState
+from radiation import Radiation
+from plotUtilities import plotHydroSolutions
+from utilityFunctions import computeRadiationVector, computeAnalyticHydroSolution
+from crossXInterface import ConstantCrossSection
+from transient import runNonlinearTransient
+
+## Derived unittest class
+#
+class TestHydroMMS(unittest.TestCase):
+   def setUp(self):
+      pass
+   def tearDown(self):
+      pass
+   def test_HydroMMS(self):
+      
+      # declare symbolic variables
+      x, t, alpha = symbols('x t alpha')
+      
+      # create solution for thermodynamic state and flow field
+      rho = exp(x+t)
+      u   = exp(-x)*sin(t) - 1
+      E   = exp(-3*alpha*t)*sin(pi*x) + 3
+      
+      # create solution for radiation field
+      psim = 0
+      psip = 0
+      
+      # numeric values
+      alpha_value = 0.01
+      cv_value    = 1.0
+      gamma_value = 1.4
+      sig_s = 1.0
+      sig_a = 0.0
+      
+      # create MMS source functions
+      rho_src, mom_src, E_src, psim_src, psip_src = createMMSSourceFunctionsRadHydro(
+         rho           = rho,
+         u             = u,
+         E             = E,
+         psim          = psim,
+         psip          = psip,
+         sigma_s_value = sig_s,
+         sigma_a_value = sig_a,
+         gamma_value   = gamma_value,
+         cv_value      = cv_value,
+         alpha_value   = alpha_value,
+         display_equations = False)
+
+      # create functions for exact solutions
+      substitutions = dict()
+      substitutions['alpha'] = alpha_value
+      rho = rho.subs(substitutions)
+      u   = u.subs(substitutions)
+      E   = E.subs(substitutions)
+      rho_f  = lambdify((symbols('x'),symbols('t')), rho,  "numpy")
+      u_f    = lambdify((symbols('x'),symbols('t')), u,    "numpy")
+      E_f    = lambdify((symbols('x'),symbols('t')), E,    "numpy")
+      psim_f = lambdify((symbols('x'),symbols('t')), psim, "numpy")
+      psip_f = lambdify((symbols('x'),symbols('t')), psip, "numpy")
+      
+      # create uniform mesh
+      n_elems = 50
+      width = 1.0
+      mesh = Mesh(n_elems, width)
+
+      # compute radiation IC
+      psi_IC = computeRadiationVector(psim_f, psip_f, mesh, t=0.0)
+      rad_IC = Radiation(psi_IC)
+
+      # compute radiation BC; assumes BC is independent of time
+      psi_left  = psip_f(x=0.0,   t=0.0)
+      psi_right = psim_f(x=width, t=0.0)
+
+      # compute hydro IC
+      hydro_IC = computeAnalyticHydroSolution(mesh,t=0.0,
+         rho=rho_f, u=u_f, E=E_f, cv=cv_value, gamma=gamma_value)
+
+      # create cross sections
+      cross_sects = [(ConstantCrossSection(sig_s, sig_s+sig_a),
+                      ConstantCrossSection(sig_s, sig_s+sig_a))
+                      for i in xrange(mesh.n_elems)]
+
+      # transient options
+      t_start  = 0.0
+      t_end = 0.1
+
+      # if run standalone, then be verbose
+      if __name__ == '__main__':
+         verbose = True
+      
+      # run the rad-hydro transient
+      rad_new, hydro_new = runNonlinearTransient(
+         mesh         = mesh,
+         problem_type = 'rad_hydro',
+         dt_option    = 'CFL',
+         CFL          = 0.5,
+         use_2_cycles = False,
+         t_start      = t_start,
+         t_end        = t_end,
+         psi_left     = psi_left,
+         psi_right    = psi_right,
+         cross_sects  = cross_sects,
+         rad_IC       = rad_IC,
+         hydro_IC     = hydro_IC,
+         mom_src      = mom_src,
+         E_src        = E_src,
+         psim_src     = psim_src,
+         psip_src     = psip_src,
+         verbose      = verbose)
+
+      # plot
+      if __name__ == '__main__':
+
+         # plot radiation solution
+
+         # compute exact hydro solution
+         hydro_exact = computeAnalyticHydroSolution(mesh, t=t_end,
+            rho=rho_f, u=u_f, E=E_f, cv=cv_value, gamma=gamma_value)
+
+         # plot hydro solution
+         plotHydroSolutions(mesh, hydro_new, exact=hydro_exact)
+
+
+# run main function from unittest module
+if __name__ == '__main__':
+   unittest.main()
+
